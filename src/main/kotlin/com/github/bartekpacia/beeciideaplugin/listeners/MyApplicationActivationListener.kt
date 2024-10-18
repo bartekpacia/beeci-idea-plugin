132cb42f77043f0bc0f1c0a43f46f88559be6f73
GitHub Action <action@github.com>
2024-10-18 23:18:10 +0000

    Template cleanup

--- src/main/kotlin/com/github/bartekpacia/beeciideaplugin/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/bartekpacia/beeciideaplugin/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.bartekpacia.beeciideaplugin.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
